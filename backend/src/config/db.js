6ff9daea9a2e79d56ea6998a8f151d2d0f78cb08
Ankit Badhautiya <anky.g13@gmail.com>
2026-06-02 20:17:34 +0530

    debug mongo connection

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -2,6 +2,14 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
+    console.log("MONGO_URI exists:", !!process.env.MONGO_URI);
+    console.log(
+      "MONGO_URI starts with:",
+      process.env.MONGO_URI
+        ? process.env.MONGO_URI.substring(0, 20)
+        : "undefined"
+    );
+
     await mongoose.connect(process.env.MONGO_URI);
 
     console.log("MongoDB Connected Successfully");
